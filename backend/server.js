afbfd22ad410aef49fb98ddfb99379cb7e7cea10
francocabrerizo <francocabrerizogoiburu@gmail.com>
2026-09-05 11:24:26 -0300

    Fix: cors edited and protected w/ a new middleware (old was deprocated)

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,10 @@ const prisma = new PrismaClient();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://bajoperfil.store', 'https://www.bajoperfil.store'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 app.use(express.json());
 
 // ==========================================
